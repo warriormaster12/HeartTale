ea234b85c5a9c8dd4201f6f8a0f07e7fe354c2b0
warriormaster12 <streng.alexander@gmail.com>
2020-11-05 13:35:40 +0200

    fixed behaviour tree

--- GameObjects/Enemy/BehaviourTree/BTRoot.gd
+++ GameObjects/Enemy/BehaviourTree/BTRoot.gd
@@ -5,35 +5,42 @@ export (NodePath) var ai_target
 onready var ai_target_node = get_node(ai_target)
 onready var active_node = get_node("Stage")
 onready var timer = get_node("BehaviourExecutionTimer")
+var can_trigger = true
 var stages = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	timer.start()
-	stages +=1
-	pass
+	timer.one_shot = true
+	
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if active_node == get_node("Stage"):
+	if active_node == get_node("Stage"):	
 		if stages == 0: 
-			timer.set_wait_time(1.0)
-		if stages  == 1 : 
-			timer.set_wait_time(5.0)
+			_wait(3.0)
+		elif stages == 1: 
+			_wait(5.0)
 			if ai_target_node.can_fire == true:
 				ai_target_node._spawn_bullets_at_once()
-			
 		elif stages == 2: 
-			timer.set_wait_time(2.0)
+			_wait(1.0)
 			ai_target_node._spawn_bullet_rotation()
-			
-		print(timer.time_left)
-	print(stages)
-	
+
+
+func _wait(wait_time):
+	if can_trigger == true:
+		timer.set_wait_time(wait_time)
+		timer.start()
+		can_trigger = false
 
 
 func _on_BehaviourExecutionTimer_timeout():
+	can_trigger = true
 	stages +=1
-	if stages == 3:
+	if stages > 2:
 		stages = 0
+
+	
