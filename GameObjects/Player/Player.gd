a423caa5f012323516fc77dd607d997cbb1db554
warriormaster12 <streng.alexander@gmail.com>
2020-11-05 10:42:22 +0200

    player can shoot. Reworking collision later

--- GameObjects/Player/Player.gd
+++ GameObjects/Player/Player.gd
@@ -5,14 +5,20 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 export var Speed = 300.0
+export var fire_delay = 0.2
 var direction = Vector2()
 onready var rotator_node = get_node("rotator")
+onready var timer = get_node("ShootingTimer")
 var bullet = preload("res://GameObjects/Enemy/Bullet.tscn")
+var can_shoot = true
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-
+	timer.set_wait_time(fire_delay)
+	timer.one_shot = true
+	
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -20,8 +26,9 @@ func _ready():
 
 func _physics_process(delta):
 	direction = Vector2()
+	rotator_node.position = self.position
 	
-	rotator_node.look_at(get_global_mouse_position())
+
 	if Input.is_action_pressed("move_r"):
 		 direction.x += 1 
 	if Input.is_action_pressed("move_l"):
@@ -30,7 +37,7 @@ func _physics_process(delta):
 		direction.y -= 1
 	if Input.is_action_pressed("move_b"):
 		direction.y += 1
-	if Input.is_action_pressed("fire"):
+	if Input.is_action_pressed("fire") && can_shoot == true:
 		_shoot()
 	
 	direction = direction.normalized() 
@@ -39,8 +46,14 @@ func _physics_process(delta):
 	direction = move_and_slide(direction)
 
 func _shoot():
-	rotator_node.position = self.position
 	var bullet_instance = bullet.instance()
+	var bullet_rotation = get_angle_to(get_global_mouse_position()) + rotator_node.get_rotation()
 	bullet_instance.position = rotator_node.position
-	bullet_instance.rotation = rotator_node.rotation
+	bullet_instance.rotation = bullet_rotation
 	get_parent().add_child(bullet_instance)
+	can_shoot = false
+	timer.start()
+
+
+func _on_ShootingTimer_timeout():
+	can_shoot = true
